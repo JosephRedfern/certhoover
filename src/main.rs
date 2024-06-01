561522d029ae3af5dfac878577582504f4d6ba5b
Joseph Redfern <joseph@redfern.me>
2024-06-01 13:25:39 +0100

    Use MPSC channel over VecDeque

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,17 @@ use clickhouse_rs::{row, Pool};
 use log::{info, warn};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
-use std::{
-    collections::VecDeque,
-    sync::{Arc, Mutex},
-    time::Duration,
-};
+use std::time::Duration;
 use tungstenite::{connect, Message};
 
 const CERTSTREAM_URL: &str = "wss://certstream.calidog.io/";
 const BATCH_SIZE: usize = 1000;
-const MAX_BATCH_AGE: Duration = Duration::new(10, 0);
+const MAX_BATCH_AGE: Duration = Duration::new(5, 0);
 
 #[tokio::main]
 async fn main() {
     // This whole thing is a bit of a monstrosity, for several reasons:
     //
-    // * I don't think a VecDeque is the best choice of data structure here, since we have to lock it every time we
-    //   want to access it.
     // * There's a hodge-podge of thread-based and async code here... the two main Clickhouse libraries I found are
     //   both async, the websocket library /can/ be async, but I don't know enough about async in rust to know whether
     //   it's performant enough -- there can be quite a few messages coming in from the websocket.
@@ -27,32 +21,27 @@ async fn main() {
 
     env_logger::init();
 
-    let ws_message_queue = Arc::new(Mutex::new(VecDeque::new()));
-    let batch_queue = Arc::new(Mutex::new(VecDeque::new()));
-
-    let ws_write_queue = Arc::clone(&ws_message_queue);
-    let ws_read_queue = Arc::clone(&ws_message_queue);
-    let batch_write_queue = Arc::clone(&batch_queue);
-    let batch_read_queue = Arc::clone(&batch_queue);
+    let (ws_sender, ws_receiver) = std::sync::mpsc::channel();
+    let (batch_sender, batch_receiver) = std::sync::mpsc::channel();
 
     // This could be a coroutine instead of a thread.
     let websocket_reader = std::thread::spawn(move || {
-        read_websocket(CERTSTREAM_URL, ws_write_queue);
+        read_websocket(CERTSTREAM_URL, ws_sender);
     });
 
     let batcher = std::thread::spawn(move || {
-        batch_records(ws_read_queue, batch_write_queue);
+        batch_records(ws_receiver, batch_sender);
     });
 
     // Inserter is async, so we don't need a thread but do need to await it.
-    insert_records(batch_read_queue).await.unwrap();
+    insert_records(batch_receiver).await.unwrap();
 
     websocket_reader.join().unwrap();
     batcher.join().unwrap();
 }
 
 async fn insert_records(
-    batch_queue: Arc<Mutex<VecDeque<Vec<TransparencyRecord>>>>,
+    batch_receiver: std::sync::mpsc::Receiver<Vec<TransparencyRecord>>,
 ) -> Result<(), Box<dyn Error>> {
     // Process batch of records for insertion.
     // Rather than deal with nested fields in the JSON, we flatten the data into a single row per domain.
@@ -64,24 +53,20 @@ async fn insert_records(
     let mut client = pool.get_handle().await?;
 
     loop {
-        // Rather than having to take the lock every time we check the queue, can we do something more efficient?
-        // Is there something signal based, or maybe a channel or something? It'd be nice if we could block here
-        // rather than this approach of taking the lock and sleeping a bit...
-        while !batch_queue.lock().unwrap().is_empty() {
-            let batch = batch_queue.lock().unwrap().pop_front().unwrap();
+        let batch = batch_receiver.recv().unwrap(); //TODO: handle error
 
-            let mut block = Block::new();
-            let mut inserted: u64 = 0;
+        let mut block = Block::new();
+        let mut inserted: u64 = 0;
 
-            for record in batch.iter() {
-                match &record.data {
-                    Some(data) => {
-                        // TODO: Can we avoid cloning here, and take ownership of the data?
-                        // Note: this is a bit of a mess, but none of the two Clickhouse libraries I found seem to support
-                        //       nested fields. We might be able to do some stuff with serdes to have it flatten the data
-                        //       for us, but I'm not sure how to do that yet...
-                        for domain in data.leaf_cert.all_domains.iter() {
-                            block.push(row!{
+        for record in batch.iter() {
+            match &record.data {
+                Some(data) => {
+                    // TODO: Can we avoid cloning here, and take ownership of the data?
+                    // Note: this is a bit of a mess, but none of the two Clickhouse libraries I found seem to support
+                    //       nested fields. We might be able to do some stuff with serdes to have it flatten the data
+                    //       for us, but I'm not sure how to do that yet...
+                    for domain in data.leaf_cert.all_domains.iter() {
+                        block.push(row!{
                                 "cert_index" => data.cert_index,
                                 "cert_link" => data.cert_link.clone(),
                                 "fingerprint" => data.leaf_cert.fingerprint.clone(),
@@ -98,64 +83,51 @@ async fn insert_records(
                                 "email_address" => data.leaf_cert.subject.email_address.clone().unwrap_or_default(),
                                 "domain" => domain.clone()
                             })?;
-                            inserted += 1;
-                        }
-                    }
-                    None => {
-                        warn!("Record missing data field: {:?}", record);
+                        inserted += 1;
                     }
                 }
+                None => {
+                    warn!("Record missing data field: {:?}", record);
+                }
             }
-            client.insert("certs", block).await.unwrap();
-
-            info!(
-                "Written batch of {} records, expanded to {} rows",
-                batch.len(),
-                inserted
-            );
         }
-        // sleep a bit... not nice.
-        std::thread::sleep(Duration::from_millis(100));
+        client.insert("certs", block).await.unwrap();
+
+        info!(
+            "Written batch of {} records, expanded to {} rows",
+            batch.len(),
+            inserted
+        );
     }
 }
 
 fn batch_records(
-    ws_message_queue: Arc<Mutex<VecDeque<String>>>,
-    batch_queue: Arc<Mutex<VecDeque<Vec<TransparencyRecord>>>>,
+    ws_read_channel: std::sync::mpsc::Receiver<String>,
+    batch_queue: std::sync::mpsc::Sender<Vec<TransparencyRecord>>,
 ) {
     // Pull from the websocket queue, batch up records, and shove them into the batch queue
     // for insertion into Clickhouse.
 
     // Would a fixed size buffer be better here? We won't always fill it but could avoid the reallocation?
     let mut message_buffer: Vec<TransparencyRecord> = Vec::new();
-
     let mut last_batch_time = std::time::Instant::now();
 
     loop {
-        while !ws_message_queue.lock().unwrap().is_empty() {
-            // TODO: there's a potential race condition here if we had multiple consumers, since we're acquiring the
-            // lock once for the length check and then again to pop the message...
-            let message = ws_message_queue.lock().unwrap().pop_front().unwrap();
-            let record: TransparencyRecord = serde_json::from_str(&message).unwrap();
+        let message = ws_read_channel.recv().unwrap(); //TODO: handle error
+        let record: TransparencyRecord = serde_json::from_str(&message).unwrap();
 
-            message_buffer.push(record);
+        message_buffer.push(record);
 
-            if message_buffer.len() >= BATCH_SIZE || last_batch_time.elapsed() >= MAX_BATCH_AGE {
-                info!("Batching up {} records", message_buffer.len());
-                batch_queue
-                    .lock()
-                    .unwrap()
-                    .push_back(message_buffer.clone());
-                message_buffer.clear();
-                last_batch_time = std::time::Instant::now();
-            }
+        if message_buffer.len() >= BATCH_SIZE || last_batch_time.elapsed() >= MAX_BATCH_AGE {
+            info!("Batching up {} records", message_buffer.len());
+            batch_queue.send(message_buffer).unwrap(); //TODO: handle error
+            message_buffer = Vec::new();
+            last_batch_time = std::time::Instant::now();
         }
-        // sleep a bit... also not nice...
-        std::thread::sleep(Duration::from_millis(100));
     }
 }
 
-fn read_websocket(url: &str, queue: Arc<Mutex<VecDeque<String>>>) {
+fn read_websocket(url: &str, ws_write_channel: std::sync::mpsc::Sender<String>) {
     loop {
         let (mut socket, _) = match connect(url) {
             Ok(result) => result,
@@ -187,7 +159,7 @@ fn read_websocket(url: &str, queue: Arc<Mutex<VecDeque<String>>>) {
             match socket.read() {
                 Ok(msg) => match msg {
                     Message::Text(text) => {
-                        queue.lock().unwrap().push_back(text);
+                        ws_write_channel.send(text).unwrap(); //todo: handle error
                     }
                     Message::Close(_) => {
                         info!("Connection closed");
